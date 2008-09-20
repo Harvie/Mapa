c5fb602b7acc8cb977aba589f8d3a14aed3b1188
Lukáš Turek <8an@praha12.net>
2008-09-20 15:03:21 +0200

    Import initial version of node monitor

---
+++ data.php
@@ -0,0 +1,41 @@
+<?php
+header('Content-Type: text/plain');
+
+try {
+	$dbh = new PDO('pgsql:dbname=map;user=www');
+	
+	$nodes_sql = "SELECT name,lat,lng FROM nodes ".
+	              "WHERE lat < ? AND lng < ? AND lat > ? AND lng > ?";
+	
+	$links_sql = "SELECT n1.lat, n1.lng, n2.lat, n2.lng FROM links ".
+	             "JOIN nodes AS n1 ON node1 = n1.id ".
+	             "JOIN nodes AS n2 ON node2 = n2.id ".
+	             "WHERE (n1.lat < ? AND n1.lng < ? AND n1.lat > ? AND n1.lng > ?) ".
+	                "OR (n2.lat < ? AND n2.lng < ? AND n2.lat > ? AND n2.lng > ?)";
+	
+	foreach (array('north','east','south','west') as $i => $var)
+		$bounds[$i] = floatval(@$_GET[$var]);
+	
+	$nodes = $dbh->prepare($nodes_sql);
+	$nodes->execute($bounds);
+	
+	$links = $dbh->prepare($links_sql);
+	$links->execute(array_merge($bounds, $bounds));
+} catch (PDOException $e) {
+    print "Database error: " . $e->getMessage();
+}
+
+?>
+{
+	"points":
+	[
+	<? foreach ($nodes as $row): ?>
+	{ label: "<?=addslashes($row['name'])?>", lat: <?=$row['lat']?>, lng: <?=$row['lng']?> },
+	<? endforeach ?>],
+	
+	"links":
+	[
+	<? foreach ($links as $row): ?>
+	{ lat1: <?=$row[0]?>, lng1: <?=$row[1]?>, lat2: <?=$row[2]?>, lng2: <?=$row[3]?> },
+	<? endforeach ?>]
+}
---
+++ map.js
@@ -0,0 +1,119 @@
+var map;
+var icons;
+
+LabeledMarker.prototype = new GMarker(new GLatLng(0, 0));
+
+function LabeledMarker(latlng, options)
+{
+	GMarker.apply(this, arguments);
+}
+
+LabeledMarker.prototype.initialize = function(map) {
+	GMarker.prototype.initialize.apply(this, arguments);
+	
+	var div = document.createElement("div");
+	div.className = "MarkerLabel";
+	div.innerHTML = this.getTitle();
+	div.style.position = "absolute";
+	map.getPane(G_MAP_MARKER_PANE).appendChild(div);
+
+	this.map = map;
+	this.div = div;
+}
+
+LabeledMarker.prototype.redraw = function(force) {
+	GMarker.prototype.redraw.apply(this, arguments);
+	
+	// We only need to do anything if the coordinate system has changed
+	if (!force) return;
+	
+	// Calculate the DIV coordinates of two opposite corners of our bounds to
+	// get the size and position of our rectangle
+	var latlng = this.getLatLng();
+	var p = this.map.fromLatLngToDivPixel(latlng);
+	var z = GOverlay.getZIndex(latlng.lat());
+	
+	// Now position our DIV based on the DIV coordinates of our bounds
+	this.div.style.left = (p.x + 7) + "px";
+	this.div.style.top = (p.y - 7) + "px";
+	this.div.style.zIndex = z - 100000; //Put it behind marker
+}
+
+LabeledMarker.prototype.remove = function()
+{
+	this.div.parentNode.removeChild(this.div);
+	this.div = null;
+	GMarker.prototype.remove.apply(this, arguments);
+}
+
+function readData(doc)
+{
+	var jsonData = eval('(' + doc + ')');
+	map.clearOverlays();
+	
+	for (i in jsonData.points)
+	{
+		var point = jsonData.points[i];
+ 		var latlng = new GLatLng(point.lat, point.lng); 
+ 		var options = { icon: icons[0], title: point.label };
+		var marker = new LabeledMarker(latlng, options);
+		map.addOverlay(marker);
+	}
+	
+	for (i in jsonData.links)
+	{
+		var link = jsonData.links[i];
+		var latlng1 = new GLatLng(link.lat1, link.lng1); 
+		var latlng2 = new GLatLng(link.lat2, link.lng2); 
+		var linePoints = [ latlng1, latlng2 ];
+		map.addOverlay(new GPolyline(linePoints, "black", 5, 1));
+		map.addOverlay(new GPolyline(linePoints, "#AAAAAA", 3, 1));
+	}
+}
+
+function loadData()
+{
+	icons = new Array();
+	icons[0] = new GIcon();
+	icons[0].image = "/images/ap.png";
+	icons[0].iconSize = new GSize(15,15);
+	icons[0].iconAnchor = new GPoint(7,7);
+	
+	var bounds = map.getBounds();
+	var north = bounds.getNorthEast().lat();
+	var east = bounds.getNorthEast().lng();
+	var south = bounds.getSouthWest().lat();
+	var west = bounds.getSouthWest().lng();
+	
+	var query = "?north=" + north + "&east=" + east + "&south=" + south + "&west=" + west;
+	GDownloadUrl("data.php" + query, readData);
+}
+
+function mapMoved()
+{
+	loadData();
+}
+
+function showMap()
+{
+	map = new GMap2(document.getElementById("map"));
+	map.setCenter(new GLatLng(50.006915, 14.422809), 14);
+	
+	map.addControl(new GLargeMapControl());
+	map.addControl(new GMapTypeControl());
+	map.addControl(new GOverviewMapControl());
+	map.addControl(new GScaleControl());
+	
+	map.setMapType(G_SATELLITE_MAP);
+	map.enableScrollWheelZoom();
+	map.enableDoubleClickZoom();
+	
+	mapMoved();
+	GEvent.addListener(map, "moveend", mapMoved);
+}
+
+function initialize()
+{
+	if (GBrowserIsCompatible())
+		showMap();
+}
