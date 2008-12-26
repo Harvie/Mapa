df3f49c5c9457a4b7492aa5f8ed6a44ab30eb2d2
Lukáš Turek <8an@praha12.net>
2008-12-26 21:06:51 +0100

    Make Google Maps API Key a configuration parameter

--- www/pages/index.php
+++ www/pages/index.php
@@ -1,14 +1,18 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+$jsFiles = glob("scripts/*.js");
+$gmapKey = Config::$gmapKey;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title>CZFree Node Monitor</title>
     <link rel="stylesheet" type="text/css" href="map.css" />
-    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAuLM8y0Dds7coOFGVF0nqtxQaDGRBdHCxKyYKLoyuPujpiM7ZexRELZc4fv7XPdRV7-aCrXnEd_-f2A"
-      type="text/javascript"></script>
-    <? foreach (glob("scripts/*.js") as $file): ?>
+    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?=$gmapKey?>" type="text/javascript"></script>
+<? foreach ($jsFiles as $file): ?>
     <script type="text/javascript" src="<?=$file?>"></script>
-    <? endforeach ?>
+<? endforeach ?>
   </head>
   
   <body onload="CzfMain.start('map','panel')" onunload="CzfMain.stop()">
