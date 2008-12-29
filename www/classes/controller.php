3551d35d459c3d49a29f81e95dee2f3144cdc854
Lukáš Turek <8an@praha12.net>
2008-12-29 21:35:42 +0100

    Convert null to empty string in JSON (instead of undefined)

--- www/classes/controller.php
+++ www/classes/controller.php
@@ -44,18 +44,16 @@ class Controller
 	protected static function convertChangeInfo(&$info)
 	{
 		if ($info['created_by'] !== null)
-		{
 			$info['created'] = self::makeChangeInfo($info['created_on'], $info['created_by']);
-			unset($info['created_on']);
-			unset($info['created_by']);
-		}
+		
+		unset($info['created_on']);
+		unset($info['created_by']);
 		
 		if ($info['changed_by'] !== null)
-		{
 			$info['changed'] = self::makeChangeInfo($info['changed_on'], $info['changed_by']);
-			unset($info['changed_on']);
-			unset($info['changed_by']);
-		}
+		
+		unset($info['changed_on']);
+		unset($info['changed_by']);
 	}
 	
 	protected static function getLanguages()
@@ -98,7 +96,7 @@ class Controller
 		if (is_numeric($data))
 			return $data;
 		
-		if (is_string($data))
+		if (is_string($data) && is_null($data))
 			return '"' . self::escape($data) . '"';
 	}
 	
