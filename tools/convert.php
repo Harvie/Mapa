f7f7d4c2b108a960a3982fa5d9194ddc6a379049
Lukáš Turek <8an@praha12.net>
2009-01-06 21:36:15 +0100

    Better conversion of node_history

--- tools/convert.php
+++ tools/convert.php
@@ -2,6 +2,7 @@
 <?php
 
 error_reporting(E_ALL);
+ini_set('memory_limit', 32*1024*1024);
 
 $mysql = new PDO('mysql:dbname=czfreemapa', 'mapa', 'XXXXXXXX');
 $mysql->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
@@ -11,6 +12,9 @@ $mysql->query('SET NAMES latin1');
 $pgsql = new PDO('pgsql:dbname=map');
 $pgsql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pgsql->beginTransaction();
+
+
+// Import nodes (including deleted)
 
 $pgsql->query('DELETE FROM nodes');
 $pgsql->query('DELETE FROM nodes_history');
@@ -40,23 +44,56 @@ foreach (array(array('node', 'nodes'), array('node_deleted', 'nodes_history')) a
 	}
 }
 
+
+// Reconstruct history of changes
+
+$select = $mysql->query('SELECT id,type,status,ownerid FROM node UNION ALL '.
+                        'SELECT id,type,status,ownerid FROM node_deleted');
+$select->setFetchMode(PDO::FETCH_ASSOC);
+foreach ($select as $row)
+	$nodes[$row['id']] = $row;
+
 $select = $mysql->query(
 	'SELECT DISTINCT h.changed_on, h.changed_by, h.nodeid, s.ownerid_before AS status,'.
-	       'IF(t.ownerid_before IS NULL, NULL, 0) AS type, o.ownerid_before AS owner_id '.
+	       'IF(t.ownerid_before IS NULL, NULL, -1) AS type, o.ownerid_before AS owner_id '.
 	'FROM node_history AS h '.
 	'LEFT JOIN (SELECT * FROM node_history WHERE value = \'S\') AS s '.
 	'ON s.changed_by = h.changed_by AND s.changed_on = h.changed_on AND s.nodeid = h.nodeid '.
 	'LEFT JOIN (SELECT * FROM node_history WHERE value = \'T\') AS t '.
 	'ON t.changed_by = h.changed_by AND t.changed_on = h.changed_on AND t.nodeid = h.nodeid '.
 	'LEFT JOIN (SELECT * FROM node_history WHERE value = \'O\') AS o '.
-	'ON o.changed_by = h.changed_by AND o.changed_on = h.changed_on AND o.nodeid = h.nodeid');
+	'ON o.changed_by = h.changed_by AND o.changed_on = h.changed_on AND o.nodeid = h.nodeid '.
+	'ORDER BY changed_on DESC');
 
 $insert = $pgsql->prepare('INSERT INTO nodes_history (changed_on,changed_by,id,status,type,owner_id) '.
                           'VALUES(?,?,?,?,?,?)');
 
 $select->setFetchMode(PDO::FETCH_NUM);
 foreach ($select as $row)
+{
+	if (!isset($nodes[$row[2]]))
+		continue;
+	
+	if ($row[0] < '2006-06-28' && $row[3] !== null)
+		if ($row[3] < 40)
+		{
+			$row[4] = $row[3];
+			$row[3] = 1;
+		}
+		else
+			$row[4] = 1;
+	
+	foreach (array(3 => 'status', 4 => 'type', 5 => 'ownerid') as $i => $col)
+		if ($row[$i] === null)
+			$row[$i] = $nodes[$row[2]][$col];
+		else
+			$nodes[$row[2]][$col] = $row[$i];
+	
 	$insert->execute($row);
+}
+
+
+// Import links
 
 $pgsql->query('DELETE FROM links');
 $pgsql->query('DELETE FROM links_history');
