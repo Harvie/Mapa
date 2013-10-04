502e5f49ac278be6ac494078ebb336154cb1dc45
Lukáš Turek <8an@praha12.net>
2013-10-05 01:14:14 +0200

    Add support for HTTPS access

--- www/pages/index.php
+++ www/pages/index.php
@@ -14,7 +14,8 @@ $config = array(
 );
 
 $jsFiles = glob("scripts/*.js");
-$gmapKey = Config::$gmapKeys[$_SERVER['SERVER_NAME']];
+$gMapURL = ($_SERVER['HTTPS'] ? 'https://' : 'http://') .
+           'maps.google.com/maps?file=api&v=2&key=' . Config::$gMapKey;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -22,7 +23,7 @@ $gmapKey = Config::$gmapKeys[$_SERVER['SERVER_NAME']];
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title>CZFree Node Monitor</title>
     <link rel="stylesheet" type="text/css" href="map.css" />
-    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?=$gmapKey?>" type="text/javascript"></script>
+    <script src="<?=htmlentities($gMapURL)?>" type="text/javascript"></script>
 <? foreach ($jsFiles as $file): ?>
     <script type="text/javascript" src="<?=$file?>"></script>
 <? endforeach ?>
